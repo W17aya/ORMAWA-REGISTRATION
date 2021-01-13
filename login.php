6c302ea0405240e4a10d62d5cfa4070ed109b257
Eko Wijaya <ekowijayawindows@gmail.com>
2021-01-13 22:41:23 +0700

    add login & index

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit-no">
+    <title>Login Aplikasi</title>
+
+    <link href="bootstrap/css/bootstrap-grid.min.css" rel="stylesheet">
+</head>
+
+<body class="H-100 bg-info d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-4 mx-auto bg-light p-4">
+
+                <h3 class="text-center text-info pb-3 mb-3 border-bottom">Login Aplikasi</h3>
+
+                <form method="post" action="ceklogin.php">
+                    <input class="form-control form-control-lg mb-3" type="text" placeholder="Username" name="username">
+                    <input class="form-control form-control-lg mb-3" type="password" placeholder="Password" name="password">
+                    <input class="btn btn-info btn-lg btn-block" type="submit" value="Login">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+<body>
+
+</body>
+
+</html>
